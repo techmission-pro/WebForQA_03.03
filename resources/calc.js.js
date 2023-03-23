437ee0c6e1177ded20f6d7dcaff5600388a41fee
Andrei Railean <andreirailian91@gmai.com>
2023-03-23 22:03:33 +0200

    calculator.

---
+++ resources/calc.js.js
@@ -0,0 +1,14 @@
+document.querySelector('.calc-calculate').addElementListener('click',function (){
+const num1 = +document.querySelerctor{'[name="num1"]'}.value;
+const num2 =+document.querySelerctor{'[name="num2"]'}.value;
+const sum = num1 + num2;
+const sum = num1 - num2;
+const sum = num1 * num2;
+const sum = num1 / num2;
+document.querySelector ('.calc-result').value=sum;
+}); 
+
+
+
+                                                                              
+                            
